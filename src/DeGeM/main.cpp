626603e4916081e0b642ee5d008aae553b92c47d
Alain Vaucher <acvaucher@gmail.com>
2017-08-05 17:55:32 +0200

    Register the class for qml

--- src/DeGeM/main.cpp
+++ src/DeGeM/main.cpp
@@ -1,9 +1,11 @@
+#include "ExpenseSummaryModel.h"
 #include <QtGui/QGuiApplication>
 #include <QtQuick/QQuickView>
 #include <QtCore/QUrl>
 
 int main(int argc, char* argv[]) {
   QGuiApplication app(argc, argv);
+  qmlRegisterType<DeGeM::ExpenseSummaryModel>("Local", 1, 0, "ExpenseSummaryModel");
 
   QQuickView view;
   view.setResizeMode(QQuickView::SizeRootObjectToView);
