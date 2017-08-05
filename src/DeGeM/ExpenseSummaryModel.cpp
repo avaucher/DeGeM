ae7316edd296aa169dbfe84d57a319f078893174
Alain Vaucher <acvaucher@gmail.com>
2017-08-05 18:13:41 +0200

    Minimal implementation of ExpenseSummaryModel

--- src/DeGeM/ExpenseSummaryModel.cpp
+++ src/DeGeM/ExpenseSummaryModel.cpp
@@ -2,6 +2,36 @@
 
 namespace DeGeM {
 
+ExpenseSummaryModel::ExpenseSummaryModel(QObject* parent)
+  : QAbstractListModel(parent) {
+  randomExpenses_.emplace_back("John", -2.0);
+  randomExpenses_.emplace_back("Julia", 12.0);
+  randomExpenses_.emplace_back("Anna", -10.0);
+}
 
+int ExpenseSummaryModel::rowCount(const QModelIndex& parent) const {
+  return static_cast<int>(randomExpenses_.size());
+}
+
+QVariant ExpenseSummaryModel::data(const QModelIndex& index, int role) const {
+  if (!index.isValid() || index.row() < 0)
+    return QVariant();
+
+  if (index.row() >= static_cast<int>(randomExpenses_.size())) {
+    return QVariant();
+  }
+
+  const auto& expense = randomExpenses_[index.row()];
+  switch (role) {
+    case PersonNameRole:
+      return QString::fromStdString(expense.getPersonName());
+    case DueAmountRole:
+      return expense.getDueAmount();
+    default:
+      break;
+  }
+
+  return QVariant();
+}
 
 } // namespace DeGeM
--- src/DeGeM/ExpenseSummaryModel.h
+++ src/DeGeM/ExpenseSummaryModel.h
@@ -1,15 +1,37 @@
 #ifndef DEGEM_EXPENSESUMMARYMODEL_H
 #define DEGEM_EXPENSESUMMARYMODEL_H
 
+#include "SingleExpenseSummary.h"
 #include <QAbstractListModel>
+#include <QtQml/QtQml>
+#include <vector>
 
 namespace DeGeM {
 
-class ExpenseSummaryModel : public QAbstractListModel {
-  Q_OBJECT
+/*!
+ * Summary class for the total expenses for the different persons.
+ */
+class ExpenseSummaryModel: public QAbstractListModel {
+ Q_OBJECT
 
+ public:
+  using Container = std::vector<SingleExpenseSummary>;
+  enum {
+    PersonNameRole = Qt::UserRole + 1,
+    DueAmountRole
+  };
+
+  explicit ExpenseSummaryModel(QObject* parent = nullptr);
+
+  int rowCount(const QModelIndex& parent) const override;
+  QVariant data(const QModelIndex& index, int role) const override;
+
+ private:
+  Container randomExpenses_;
 };
 
 } // namespace DeGeM
+
+QML_DECLARE_TYPE(DeGeM::ExpenseSummaryModel)
 
 #endif // DEGEM_EXPENSESUMMARYMODEL_H
